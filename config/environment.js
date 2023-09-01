a4d77007409f198799b2467c938b88b135e16786
Nabuhotnii Roman <roman.nabukhotnyi@globallogic.com>
2023-09-01 19:35:30 +0300

    update config

--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function (environment) {
     rootURL: '/',
     locationType: 'history',
     EmberENV: {
+      RAISE_ON_DEPRECATION: true,
       EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -43,6 +44,8 @@ module.exports = function (environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
   }
+
+  ENV.MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN;
 
   return ENV;
 };
