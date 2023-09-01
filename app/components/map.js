ddf12c739ca6d4a6295429c2b7e0ced918732f70
Nabuhotnii Roman <roman.nabukhotnyi@globallogic.com>
2023-09-01 21:16:07 +0300

    add api and model

--- app/components/map.js
+++ app/components/map.js
@@ -5,9 +5,9 @@ const MAPBOX_API = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static';
 
 export default class MapComponent extends Component {
   get src() {
-    let { width, height, zoom } = this.args;
+    let { lng, lat, width, height, zoom } = this.args;
 
-    let coordinates = `-122.4184,37.7797,${zoom}`;
+    let coordinates = `${lng},${lat},${zoom}`;
     let dimensions = `${width}x${height}`;
     let accessToken = `access_token=${this.token}`;
 
